9dfeb53c34e90e463e64a7fdae7c17abdacea0a4
Azam <iamazamafridi@gmail.com>
2023-10-13 23:24:36 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# ASL (American Sign Language) Translation Web Application
+
+## Overview
+
+This is a web application that uses deep learning to translate American Sign Language (ASL) into english alphabets. The application is built using Flask, a Python web framework,OPENCV, a Python framework for computer vision related tasks and leverages deep learning models to interpret and generate ASL signs.
+
+## Features
+
+- **ASL TO ENGLISH ALPHABET**
+
+## Getting Started
+
+### Prerequisites
+
+Before you can run this application, ensure you have the following installed:
+
+- Python (3.6+)
+- Flask
+- Deep learning libraries (e.g., TensorFlow, PyTorch)
+
+### Installation
+
+1. Clone this repository to your local machine:
+2. Make sure that you have a working web cam available.
+3. Open your terminal/cmd and go to the cloned repository.
+4. Type "Python app.py" and run.
+5. Open your browser and go to "http://127.0.0.1:5000/"
---
+++ model.py
@@ -0,0 +1,136 @@
+'''
+Live model testing
+'''
+
+import numpy as np
+import cv2
+import mediapipe as mp
+# import skimage
+# from skimage.transform import resize
+
+# import tensorflow and keras
+import tensorflow as tf
+from tensorflow import keras
+# import os
+
+print("Packages imported...")
+
+
+class handTracker():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.modelComplex = modelComplexity
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def handsFinder(self, image, draw=True):
+        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imageRGB)
+
+        bbox = None  # Variable to store the bounding box coordinates
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
+
+                # Calculate bounding box coordinates
+                x_min, y_min, x_max, y_max = self.calculate_bounding_box(handLms, image)
+
+                # Store the bounding box coordinates
+                bbox = (x_min, y_min, x_max, y_max)
+
+        return image, bbox
+    
+    def calculate_bounding_box(self, handLms, image):
+        x_list = []
+        y_list = []
+        for lm in handLms.landmark:
+            h, w, _ = image.shape
+            x, y = int(lm.x * w), int(lm.y * h)
+            x_list.append(x)
+            y_list.append(y)
+        x_min, x_max = min(x_list), max(x_list)
+        y_min, y_max = min(y_list), max(y_list)
+        return x_min, y_min, x_max, y_max
+
+def for_min(val):
+    while (val<=0):
+        val+=5
+    return val
+
+def for_max(val,maxi):
+    while(val>=maxi):
+        val-=5
+    return val
+
+
+
+
+labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+           'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+
+
+from keras.models import load_model
+model = load_model("asl_model2.h5")
+print(model.summary())
+    
+def main():
+    cap = cv2.VideoCapture(0)
+    tracker = handTracker()
+
+    while True:
+        success, image = cap.read()
+        image_copy = image.copy()
+
+        image, bbox = tracker.handsFinder(image)
+        height, width, _ = image.shape
+        #print("Image size - Width: {} pixels, Height: {} pixels".format(width, height))
+
+        if bbox is not None:
+            x_min, y_min, x_max, y_max = bbox
+
+            x_min = for_min(x_min)
+            y_min = for_min(y_min)
+            x_max = for_max(x_max,width)
+            y_max = for_max(y_max,height)
+
+             
+            print(f'y_min = {y_min} , y_max = {y_max} , x_min = {x_min} , xmax = {x_max}')
+            
+            hand_image_for_model = image[y_min:y_max, x_min:x_max]
+            #cv2.imshow("Hand Image", hand_image_for_model)
+
+            resized_image = cv2.resize(hand_image_for_model, (64, 64))
+            input_image = np.expand_dims(resized_image, axis=0)
+
+            # # Normalize the input image
+            input_image = input_image.astype('float32') / 255.0
+
+            # # Make the prediction
+            prediction = model.predict(input_image)
+
+            # # # Get the predicted class
+            predicted_index = np.argmax(prediction)
+
+            predicted_class = labels[predicted_index]
+            print('Predicted class:', predicted_class)
+            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            cv2.putText(image, predicted_class, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    
+        cv2.imshow("Video", image)
+        delay = 1 # use 1
+        if cv2.waitKey(delay) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
